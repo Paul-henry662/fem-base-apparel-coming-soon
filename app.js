271d6bdcd6d97fdf9909a0df324e15b85298535f
Paul-henry Ngounou <ngpaulhenry@gmail.com>
2021-07-15 06:50:26 +0100

    script ok

--- app.js
+++ app.js
@@ -3,3 +3,17 @@
  * Main script file.
  */
 
+const emailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
+
+function validateForm(){
+    const form = document.querySelector(".main__form");
+    const emailInput = document.querySelector(".main__form__input").value;
+
+    if(emailRegex.test(emailInput)){
+        return true;
+    }
+    else{
+        form.classList.add("main__form--error");
+        return false;
+    }
+}
